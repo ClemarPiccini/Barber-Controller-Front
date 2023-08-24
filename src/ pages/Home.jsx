8b2b80d67a6ca0d833e2ca995b98ecd1e5c6a916
ClemarPiccini <cj.piccini@gmail.com>
2023-08-24 12:44:38 -0300

    att css

--- src/ pages/Home.jsx
+++ src/ pages/Home.jsx
@@ -1,18 +1,36 @@
 import React, { useState } from 'react';
-import './styles.css';
+import axios from 'axios';
+import { Navigate } from 'react-router-dom';
 
 function Home() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [redirectToDashboard, setRedirectToDashboard] = useState(false); 
 
-  const handleLogin = () => {
-    // Perform your login logic here
-    console.log('Username:', username);
-    console.log('Password:', password);
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:3001/login', {
+        email: username, 
+        senha: password,
+      });
+
+      // Verificar a resposta do servidor e tomar as ações necessárias
+      if (response.data.token) {
+        console.log('Login bem-sucedido');
+        setRedirectToDashboard(true); 
+      } else {
+        console.log('Credenciais inválidas');
+      }
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+    }
   };
+  if (redirectToDashboard) {
+    return <Navigate to="/dashboard" replace />;
+  }
 
   return (
-    <div class="principal">
+    <div className="principal">
       <h1>Bem-vindo à Barber Controller</h1>
       <p>Seu sistema de gerenciamento de barbearia</p>
 
@@ -23,6 +41,7 @@ function Home() {
             <label>Usuário:</label>
             <input
               type="text"
+              name="username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
@@ -31,6 +50,7 @@ function Home() {
             <label>Senha: </label>
             <input
               type="password"
+              name="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
